fc1a58f4022d6c8be314dff82bdae85e246b3fd7
Saar Shen <saars@microsoft.com>
2025-07-15 15:01:00 -0700

    Add json properties for options

--- src/Areas/AppInsightsProfiler/Options/AppInsightsProfilerOptions.cs
+++ src/Areas/AppInsightsProfiler/Options/AppInsightsProfilerOptions.cs
@@ -1,25 +1,30 @@
+using System.Text.Json.Serialization;
 using AzureMcp.Options;
 
 namespace AzureMcp.Areas.AppInsightsProfiler.Options;
 
 public class AppInsightsProfilerOptions : SubscriptionOptions
 {
+    [JsonPropertyName("base-url")]
     /// <summary>
     /// The base URL for the App Insights Profiler API.
     /// </summary>
     public string BaseUrl { get; set; } = "https://api.appinsightsprofiler.azure.com/";
 
-    /// <summary>
+    [JsonPropertyName("app-id")]
+   /// <summary>
     /// Gets or sets the app ids of the application insights components.
     /// </summary>
     public Guid AppId { get; set; }
 
+    [JsonPropertyName("start-date-time-utc")]
     /// <summary>
     /// Gets or sets the start date time for the insights query.
     /// Defaults to 24 hours ago.
     /// </summary>
     public DateTime StartDateTimeUtc { get; set; } = DateTime.UtcNow.AddDays(-1);
 
+    [JsonPropertyName("end-date-time-utc")]
     /// <summary>
     /// Gets or sets the end date time for the insights query.
     /// Defaults to the current time.
