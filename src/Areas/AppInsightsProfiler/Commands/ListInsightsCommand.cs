5e2d4cecc05100c1fc99a72bf5d4ec1b6de1dbee
Saar Shen <saars@microsoft.com>
2025-07-15 16:19:02 -0700

    Register app id option

--- src/Areas/AppInsightsProfiler/Commands/ListInsightsCommand.cs
+++ src/Areas/AppInsightsProfiler/Commands/ListInsightsCommand.cs
@@ -6,20 +6,34 @@ using Microsoft.Extensions.Logging;
 
 namespace AzureMcp.Areas.AppInsightsProfiler.Commands;
 
-public class ListInsightsCommand : AppInsightsProfilerBaseCommand
+public class ListInsightsCommand(ILogger<ListInsightsCommand> logger) : AppInsightsProfilerBaseCommand(logger)
 {
     private const string CommandTitle = "List Code Optimization Insights";
-
-    public ListInsightsCommand(ILogger<ListInsightsCommand> logger)
-        : base(logger)
+    private readonly Option<Guid> _appIdOption = new(
+        "--app-id",
+        "The application insights resource ID to query for code optimization insights.")
     {
-    }
+        IsRequired = true
+    };
 
     public override string Name => "list-insights";
 
     public override string Description => "List code optimization insights for an application identified by the app id of the application insights resource.";
 
     public override string Title => CommandTitle;
+
+    protected override void RegisterOptions(Command command)
+    {
+        base.RegisterOptions(command);
+        command.AddOption(_appIdOption);
+    }
+
+    protected override AppInsightsProfilerOptions BindOptions(ParseResult parseResult)
+    {
+        var options = base.BindOptions(parseResult);
+        options.AppId = parseResult.GetValueForOption(_appIdOption);
+        return options;
+    }
 
     [McpServerTool(Destructive = false, ReadOnly = true, Title = CommandTitle)]
     public async override Task<CommandResponse> ExecuteAsync(CommandContext context, ParseResult parseResult)
@@ -36,7 +50,11 @@ public class ListInsightsCommand : AppInsightsProfilerBaseCommand
             context.Activity?.WithSubscriptionTag(options);
 
             IAppInsightsProfilerDataplaneService dataplane = context.GetService<IAppInsightsProfilerDataplaneService>();
-            var insights = await dataplane.GetInsightsAsync([options.AppId], options.StartDateTimeUtc, options.EndDateTimeUtc, cancellationToken: default).ConfigureAwait(false);
+            var insights = await dataplane.GetInsightsAsync(
+                [options.AppId],
+                options.StartDateTimeUtc,
+                options.EndDateTimeUtc,
+                cancellationToken: default).ConfigureAwait(false);
 
             context.Response.Results = insights?.Count > 0 ?
                 ResponseResult.Create(
